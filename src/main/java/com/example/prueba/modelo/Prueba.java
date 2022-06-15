7caf5920314e148742800ff7fb9bf8cf2d03e3af
VASS\mgomezgarrote <mgomezgarrote@serbatic.es>
2022-06-15 12:29:34 +0200

    Commit inicial

---
+++ src/main/java/com/example/prueba/modelo/Prueba.java
@@ -0,0 +1,25 @@
+package com.example.prueba.modelo;
+
+public class Prueba {
+	
+	private int prueba;
+
+	public Prueba() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Prueba(int prueba) {
+		super();
+		this.prueba = prueba;
+	}
+
+	public int getPrueba() {
+		return prueba;
+	}
+
+	public void setPrueba(int prueba) {
+		this.prueba = prueba;
+	}
+	
+}
